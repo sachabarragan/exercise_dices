4ff0ca7f785a30cd011c9c08003dcf10c8653c1d
Sacha Barragan <barragan.sacha@gmail.com>
2021-03-16 21:28:53 -0300

    Exercises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+  dadoUno: number;
+  dadoDos: number;
+  gano: boolean;
+  urlUno: string;
+  urlDos: string;
+
+  constructor(){
+    this.dadoUno = Math.floor(Math.random() * (6 - 1) + 1);
+    this.dadoDos = Math.floor(Math.random() * (6 - 1) + 1);
+    this.gano = false;
+    this.urlUno = `../assets/img/dice${this.dadoUno}.png`;
+    this.urlDos = `../assets/img/dice${this.dadoDos}.png`;
+  }
+
+  cambiarDados():void {
+    this.dadoUno = Math.floor(Math.random() * (6 - 1) + 1);
+    this.dadoDos = Math.floor(Math.random() * (6 - 1) + 1);
+    this.urlUno = `../assets/img/dice${this.dadoUno}.png`;
+    this.urlDos = `../assets/img/dice${this.dadoDos}.png`;
+
+    if(this.dadoUno === this.dadoDos){
+      this.gano = true;
+    } else {
+      this.gano = false;
+    }
+  }
 }
